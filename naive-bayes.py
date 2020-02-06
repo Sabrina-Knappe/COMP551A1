0ba84ae8e3c043012991eb4c60257f04a5eb9fa5
Sabrina Knappe <sabrina.knappe@mail.mcgill.ca>
2020-02-06 15:32:56 -0500

    Added classes for logistic regression and naive bayes

---
+++ naive-bayes.py
@@ -0,0 +1,18 @@
+#Implementing the naive bayes class
+
+class Logistic_Regression:
+  def __init__(step_size, name):
+    self.name = name
+
+    
+
+def fit(self, training_data, training_labels):
+    print("Hello my name is " + self.name)
+
+def predict(self, test_data, test_labels):
+    category= 0
+    print("We predict that "+self.name+"is category "+category)
+    return category
+
+print(p1.name)
+print(p1.age)
