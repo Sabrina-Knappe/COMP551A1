6301552a9f50d1622938f4ed3232d8268d52d67c
Sabrina-Knappe <56092221+Sabrina-Knappe@users.noreply.github.com>
2020-02-09 14:20:18 -0500

    Fix syntax errors

--- Models/naive-bayes.py
+++ Models/naive-bayes.py
@@ -1,22 +1,30 @@
 #Implementing the naive bayes class
-
-class Naive_Bayes:
-  def __init__(step_size, name, binary):
-    self.name = name
-    if(binary)      #is it a binary of multiclass thing
-        self.type==True
-    else
-        self.type==False
+class Naive_Bayes(object):
+    Type=0
+    
+    def __init__(self, step_size, name, binary):
+        self.name = name
+        if(binary):       
+            self.Type==True
+        else:
+            self.Type==False
     
 
 def fit(self, training_data, training_labels):
+    model= 0
     print("Hello my name is " + self.name)
+
+
+    return model
+
+
+#class prior
+
+
+#likelihood
 
 
 def predict(self, test_data, test_labels):
     category= 0
-    print("We predict that "+self.name+"is category "+category)
+    print("We predict that "+self.name+"has label "+category)
     return category
-
-print(p1.name)
-print(p1.age)
