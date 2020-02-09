a18595718dcfbb9d363f0feff47a619222612d7b
Yujing Zou <yujing.zou@mail.mcgill.ca>
2020-02-08 19:26:55 -0500

    Update data-preprocess_adultData_Yujing.py

--- data-preprocess_adultData_Yujing.py
+++ data-preprocess_adultData_Yujing.py
@@ -10,7 +10,6 @@ Extraction was done by Barry Becker from the 1994 Census database.
 Prediction task is to determine whether a person makes over 50K a year.
 
 
-- Yujing Zou
 """
 
 # Import package
