b1660a422ca3f5c3ec4ed2a6dc9c27b18b58c5e0
Sabrina-Knappe <56092221+Sabrina-Knappe@users.noreply.github.com>
2020-02-09 15:44:46 -0500

    Update naive-bayes.py

--- Models/naive-bayes.py
+++ Models/naive-bayes.py
@@ -1,24 +1,38 @@
 #Implementing the naive bayes class
 class Naive_Bayes(object):
-    Type=0
+    feature_types=[]
+    prior_type=""
+    name=""
     
-    def __init__(self, step_size, name, binary):
+    def __init__(self, step_size, name, feature_types, prior_type):
         self.name = name
-        if(binary):       
-            self.Type==True
-        else:
-            self.Type==False
+        self.prior_type= prior_type
+
     
 
 def fit(self, training_data, training_labels):
     model= 0
+    prior= 0
     print("Hello my name is " + self.name)
-
-
+    #learn the prior probabilities
+    if(self.prior_type=="multiclass"):
+        prior=self.multiclass(training_data, training_labels)
+    else:
+        prior=self.binary(training_data, training_labels)
+    #learn the likelihood components
+    if()
     return model
 
 
 #class prior
+def multiclass(self, training_data, training_labels):
+    #use the multi version of max likelihood??
+    return 0
+
+
+def binary(self, training_data, training_labels):
+    #use the max likelihood estimate
+    return 0
 
 
 #likelihood
