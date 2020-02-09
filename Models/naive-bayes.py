4bb4c70567ce222f70674896f3c27f4c3a1cef96
Sabrina-Knappe <56092221+Sabrina-Knappe@users.noreply.github.com>
2020-02-09 16:21:40 -0500

    Update naive-bayes.py

--- Models/naive-bayes.py
+++ Models/naive-bayes.py
@@ -7,6 +7,7 @@ class Naive_Bayes(object):
     def __init__(self, step_size, name, feature_types, prior_type):
         self.name = name
         self.prior_type= prior_type
+        self.feature_types= feature_types
 
     
 
@@ -18,11 +19,26 @@ def fit(self, training_data, training_labels):
     if(self.prior_type=="multiclass"):
         prior=self.multiclass(training_data, training_labels)
     else:
-        prior=self.binary(training_data, training_labels)
+        prior=self.binary_prior(training_data, training_labels)
     #learn the likelihood components
-    if()
+    i=0
+    for t in training_data:
+        if(self.feature_types[i]=="binary"):
+            self.binary_likelihood()
+        elif(self.feature_types[i]==""):
+            self.categorical
+        else:
+            self.continuous
+
+
+
     return model
 
+
+def predict(self, test_data, test_labels):
+    category= 0
+    print("We predict that "+self.name+"has label "+category)
+    return category
 
 #class prior
 def multiclass(self, training_data, training_labels):
@@ -30,15 +46,12 @@ def multiclass(self, training_data, training_labels):
     return 0
 
 
-def binary(self, training_data, training_labels):
+def binary_prior(self, training_data, training_labels):
     #use the max likelihood estimate
     return 0
 
 
 #likelihood
+def binary_likelihood(self, training_data, training_labels):
 
-
-def predict(self, test_data, test_labels):
-    category= 0
-    print("We predict that "+self.name+"has label "+category)
-    return category
+    return 0
