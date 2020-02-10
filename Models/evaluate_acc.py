ffde72e862fa0ccf127c44b323f002c8a852414c
Yujing Zou <yujing.zou@mail.mcgill.ca>
2020-02-10 02:03:57 -0500

    Update evaluate_acc.py

--- Models/evaluate_acc.py
+++ Models/evaluate_acc.py
@@ -22,3 +22,31 @@ def evaluate_acc(y,y_hat):
     '''
 # Accuracy, Recall, Precision, F1 Score in Python from scratch
     # https://www.youtube.com/watch?v=9PbrWiLC-4k
+    
+# Function to calculate mean absolute error
+def mae(y_true, y_pred):
+    return np.mean(abs(y_true - y_pred))
+
+# Takes in a model, trains the model, and evaluates the model on the test set
+def fit_and_evaluate(model):
+    
+    # Train the model
+    model.fit(X_train, y_train)
+    
+    # Make predictions and evalute
+    model_pred = model.predict(X_test)
+    model_mae = mae(y_test, model_pred)
+    
+    # Return the performance metric
+    return model_mae
+
+
+
+### classification metrics - Accuracy, Recall, Precision, F1 Score
+
+    
+
+
+
+
+
