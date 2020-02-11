2ce596950e40b0b2ea262677e9b21d33eca132bb
Yujing Zou <yujing.zou@mail.mcgill.ca>
2020-02-11 02:31:22 -0500

    updated immuno dataset 4

--- Data_Preprocessing/data_preprocess_immuno.py
+++ Data_Preprocessing/data_preprocess_immuno.py
@@ -13,6 +13,7 @@ import numpy as np
 import os 
 from sklearn.model_selection import train_test_split
 from kfold_CV_try import kfold_cross_validation
+from kfold_CV_try import train_validation_split
 
 dataFolderPath = 'D:\\Documents\\U4 Academics\\Winter 2020\\COMP 551-Applied Machine Learning\\Assignments\\Assignment 1\\COMP551A1\\Dataset_Folder\\Dataset4_Immunotherapy'
 os.chdir (dataFolderPath)
@@ -38,30 +39,19 @@ y_im = yTrain_im
 xy_conc_im= np.column_stack((x_im,y_im)) # cannot use np.concatenate of data with different dimensions 
 dataset_im = xy_conc_im; folds = 5 # delete folds later when embedded in function input 
 folds = 5
-dataset_split_im = kfold_cross_validation(dataset_im,folds)
+dataset_split_im, cv_train_data_im,cv_train_label_im= kfold_cross_validation(dataset_im,folds)
 
-# delete the last column of each of the five fol, which is the label, while storing the label
-cv_train_data_im = []
-cv_train_label_im = []; label_ind_im = len(xy_conc_im[1,:])-1
-for i in range (folds): 
-    # access the last label column of matrix 
-    aa = dataset_split_im[i]
-    bb = np.array(aa)
-    label = bb [:,label_ind_im]
-    label_array = np.array(label)
-    cv_train_label_im.append(label)
-    # delete the last label column of each matrix fold 
-    b = np.delete(dataset_split_im[i],label_ind_im,1) # 1 means column 
-    cv_train_data_im.append(b)
     
 # cv_train_data has k (5) folds including the 4 folds for training set and 1 fold for testing set 
 # cv_train_label has k (5) folds for the corresponding labels for the 5 folds in cv_train_data
-    
+  
 # training the model k times so change the index of training_data and training_labels 
-
-training_data_im = cv_train_data_im[0]
-training_labels_im = cv_train_label_im[0]
-
+# training data: concatenate k-1 folds, validation data for 1 fold: evaluate for k times, uncomment the rest of the lines when testing 
+validate_data_im,validate_labels_im,training_data_im,training_labels_im = train_validation_split(cv_train_data_im,cv_train_label_im,1)
+#validate_data_im,validate_labels_im,training_data_im,training_labels_im = train_validation_split(cv_train_data_im,cv_train_label_im,2)
+#validate_data_im,validate_labels_im,training_data_im,training_labels_im = train_validation_split(cv_train_data_im,cv_train_label_im,3)
+#validate_data_im,validate_labels_im,training_data_im,training_labels_im = train_validation_split(cv_train_data_im,cv_train_label_im,4)
+#validate_data_im,validate_labels_im,training_data_im,training_labels_im = train_validation_split(cv_train_data_im,cv_train_label_im,5)
 
 ## LOGISTIC REGRESSION MODEL IMPLEMENTATION AND TESTING 
 dataFolderPath = 'D:\\Documents\\U4 Academics\\Winter 2020\\COMP 551-Applied Machine Learning\\Assignments\\Assignment 1\\COMP551A1\\Models'
@@ -70,7 +60,9 @@ os.chdir (dataFolderPath)
 import logisticRegression as lr
   
 immuno_lr = lr.Logistic_Regression(0.02,"immunotherapy","binary")
-params = lr.fit(immuno_lr,training_data_im, training_labels_im, 0.01, 1e-2)
+immuno_params = lr.fit(immuno_lr,training_data_im, training_labels_im, 0.01, 1e-2)
+# prediction 
+immuno_pred = lr.predict(immuno_lr,immuno_params,xTest_im)
 
 
 
