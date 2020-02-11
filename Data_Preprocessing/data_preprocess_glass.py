45faa437a9f141c1763a40e779d385f18b3aa995
Sabrina Knappe <sabrina.knappe@mail.mcgill.ca>
2020-02-10 22:44:47 -0500

    fixed naive bayes

--- Data_Preprocessing/data_preprocess_glass.py
+++ Data_Preprocessing/data_preprocess_glass.py
@@ -4,14 +4,16 @@ import os
 
 # def preprocess():
     #put the data into an array
-glass_data= np.genfromtxt('../Dataset_Folder/glass.data', delimiter=',')
-print(glass_data)
 
-    #split into design matrix and labels
-glass_labels = glass_data[:, 10]
-print(glass_labels)
+def preprocess():
+    glass_data= np.genfromtxt('Dataset_Folder/glass.data', delimiter=',')
+    print(glass_data)
 
-glass_design_matrix= np.delete(glass_data, 10, 1)
-print(glass_design_matrix)
+        #split into design matrix and labels
+    glass_labels = glass_data[:, 10]
+    print(glass_labels)
 
-return glass_design_matrix, glass_labels
+    glass_design_matrix= np.delete(glass_data, 10, 1)
+    print(glass_design_matrix)
+
+    return glass_design_matrix, glass_labels
